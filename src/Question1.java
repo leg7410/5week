8cc9ce92342c7e3da5f0a183c7c65c4a13892cc6
user <202430325@email.daelim.ac.kr>
2024-10-02 16:48:47 +0900

    d

--- src/Question1.java
+++ src/Question1.java
@@ -1,15 +1,21 @@
-import java.util.Scanner;
-
 public class Question1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = sc.nextInt();
-
-        if (number % 2 == 0) {
-            System.out.println(number + " 짝수 입니다.");
-        } else {
-            System.out.println(number + " 홀수 입니다.");
+        for(int i=1; i<=9; i++){
+            System.out.println("7 * "+i+" = "+7*i);
         }
     }
 }
+
+/*
+public class Question1 {
+    public static void main(String[] args) {
+        for(int i=2; i<=9; i++){
+            for(int j=1; j<=9; j++){
+                System.out.print(i+" * "+j+" = "+j*i +"\t");
+            }
+            System.out.println("");
+
+        }
+    }
+}
+ */
